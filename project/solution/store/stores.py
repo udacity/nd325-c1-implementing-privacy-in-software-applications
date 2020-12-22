b8abfa6532788408917bd220feaaa77adf922cdc
Mihir Patil <mihir.patil@berkeley.edu>
2020-12-21 16:34:48 -0800

    Added candidate store changes, and first and last names

--- project/solution/store/stores.py
+++ project/solution/store/stores.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,10 +8,21 @@ Base = declarative_base()
 class StoredVoter(Base):
     __tablename__ = 'voter'
     obfuscated_voter_id = Column(String, primary_key=True)
+
+    # First and Last names are necessary for fraud enforcement
+    first_name = Column(String)
+    last_name = Column(String)
     status = Column(String)
 
 
 class StoredBallot(Base):
     __tablename__ = 'ballot'
     ballot_number = Column(String, primary_key=True)
+    chosen_candidate_id = Column(Integer, ForeignKey('candidate.candidate_id'))
     status = Column(String)
+
+
+class StoredCandidate(Base):
+    __tablename__ = 'candidate'
+    candidate_id = Column(Integer, primary_key=True, autoincrement=True)
+    candidate_name = Column(String)
