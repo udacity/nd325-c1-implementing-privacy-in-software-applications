5a7f24dacde2326ae5c5e25a8cc5c94e0caea36b
Mihir Patil <mihir.patil@berkeley.edu>
2020-12-25 21:36:16 -0800

    Added blueprint icons

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import { H1, H4, H5, FormGroup, InputGroup, RadioGroup, Radio } from "@blueprintjs/core"
+import { H1, H4, H5, FormGroup, InputGroup, RadioGroup, Radio, Classes } from "@blueprintjs/core"
 import { IconNames } from "@blueprintjs/icons"
 
 function App() {
   return (
     <div className="app">
       <div className="app-header">
-          <H1> REPUBLIC OF MARS </H1>
-          <H4> Election of the Chancellor of the Republic </H4>
+          <H1 className="white-text"> The Republic of Mars </H1>
+          <H4 className="white-text"> DEPARTMENT OF ELECTORAL AFFAIRS </H4>
       </div>
       <div className="form">
         <FormGroup>
-            <H5>Enter your voter information</H5>
+            <H5 className="white-text">Enter your voter information</H5>
             <InputGroup large={true} leftIcon={IconNames.PERSON} placeholder="Your National ID" />
             <br />
             <InputGroup large={true} leftIcon={IconNames.DOCUMENT} placeholder="Your Ballot Number" />
             <br />
-            <H5>Choose a Candidate for Chancellor of the Republic</H5>
+            <H5 className="white-text">Choose a Candidate for Chancellor of the Republic</H5>
             <RadioGroup inline={false}>
               <Radio label="Susan Schmidt" value="Susan Schmidt" />
               <Radio label="Martin Alvarez" value="Martin Alvarez" />
