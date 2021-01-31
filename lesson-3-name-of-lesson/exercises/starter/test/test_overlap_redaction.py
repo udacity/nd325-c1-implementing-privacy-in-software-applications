b4c244dab92e94f0adb6bf36a44553fa4fed7c84
Mihir Patil <mihir.patil@berkeley.edu>
2021-01-30 17:35:23 -0800

    Overlap redaction

--- lesson-3-name-of-lesson/exercises/starter/test/test_overlap_redaction.py
+++ lesson-3-name-of-lesson/exercises/starter/test/test_overlap_redaction.py
@@ -1,7 +1,7 @@
-from main.pii_detection import redact_phone_numbers
+from main.pii_detection import redact_id_numbers
 
 
-class TestRegexRedaction:
+class TestOverlapRedaction:
 
     def test_id_number_redaction_vanilla(self):
         """
@@ -9,7 +9,7 @@ class TestRegexRedaction:
         """
         original_text = """My Id number is 12306.""".strip()
         expected_redacted_comment = """My Id number is [REDACTED ID NUMBER].""".strip()
-        assert redact_phone_numbers(original_text) == expected_redacted_comment
+        assert redact_id_numbers(original_text) == expected_redacted_comment
 
     def test_id_number_redaction_special_id_number(self):
         """
@@ -17,7 +17,7 @@ class TestRegexRedaction:
         """
         original_text = """My Id number is 125645331.""".strip()
         expected_redacted_comment = """My Id number is [REDACTED ID NUMBER].""".strip()
-        assert redact_phone_numbers(original_text) == expected_redacted_comment
+        assert redact_id_numbers(original_text) == expected_redacted_comment
 
     def test_id_number_redaction_removed_id_number(self):
         """
@@ -25,7 +25,7 @@ class TestRegexRedaction:
         """
         original_text = """The amusement park's zip code is 11305.""".strip()
         expected_redacted_comment = original_text
-        assert redact_phone_numbers(original_text) == expected_redacted_comment
+        assert redact_id_numbers(original_text) == expected_redacted_comment
 
     def test_id_number_redaction_4(self):
         """
@@ -33,6 +33,6 @@ class TestRegexRedaction:
         """
         original_text = """The amusement park's zip code is 95054.""".strip()
         expected_redacted_comment = original_text
-        assert redact_phone_numbers(original_text) == expected_redacted_comment
+        assert redact_id_numbers(original_text) == expected_redacted_comment
 
 
